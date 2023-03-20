2eab6239a41798a24411d4f83261729ab36ba80f
aSEMEGA <semegaaliou4@gmail.com>
2023-03-20 16:18:36 +0000

    boucle

---
+++ Boucles.py
@@ -0,0 +1,20 @@
+'''
+Les boucles 
+for       pour
+while     tant que
+
+
+range   plage de valeur
+'''
+
+for i in range(5):
+    print("bonjours")
+
+i = (int(input("entrer i :")))
+while i >=18:
+    print( "vous êtes majeur")
+    break
+    
+
+
+
