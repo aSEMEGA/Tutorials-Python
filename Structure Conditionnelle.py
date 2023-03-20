46a139b25828b2efb246378ffd9979b16e8209b3
aSEMEGA <semegaaliou4@gmail.com>
2023-03-20 13:07:26 +0000

    Structure Conditionnelle

---
+++ Structure Conditionnelle.py
@@ -0,0 +1,21 @@
+'''
+Strucuture Conditionnelle
+
+if   -> si
+else -> sinon
+'''
+
+age = 18
+if age >= 18:
+    print("vous êtes majeur")
+else :
+    print("vous êtes mineur")
+
+
+#on donne la main à l'utilisateur
+
+Age = (int(input("entrer votre age :\n")))
+if Age >= 18:
+    print("vous êtes majeur")
+else :
+    print("vous êtes mineur")
