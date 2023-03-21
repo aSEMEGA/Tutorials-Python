6aefd84a6c72a7ceb4d2c0e51e29d35eab4d53eb
aSEMEGA <semegaaliou4@gmail.com>
2023-03-21 14:33:08 +0000

    tuples

---
+++ tuples.py
@@ -0,0 +1,12 @@
+'''
+Structure de donnéeq : Tuples(sequence)
+-----------------------------------
+les tuples sont immuables ------>constantes
+il faut mettre une virgule derrière si une seule tuples
+'''
+montuple = (44, 1.8, "jean")
+
+print(montuple[0])
+
+(nombre1, prenom) = (11,"luc")
+print(nombre1, prenom)
