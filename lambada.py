e0d574fbe980aca8b31e641d6ccd98c5e6b0b1d5
aSEMEGA <semegaaliou4@gmail.com>
2023-03-23 14:13:11 +0000

    lambada

---
+++ lambada.py
@@ -0,0 +1,11 @@
+'''
+fonctions lambda
+syntaxe --> lambda <arg1>,<arg2>,.... : instruction
+'''
+
+carre = lambda x : x * x
+print(carre(2))
+print("---------")
+addition = lambda nb1, nb2 : nb1 +nb2
+
+print(addition(10,65))
