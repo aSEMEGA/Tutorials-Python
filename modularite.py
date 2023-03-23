b874e682810833c549801d6edeb559f1e3581040
aSEMEGA <semegaaliou4@gmail.com>
2023-03-23 14:24:09 +0000

    modularite

---
+++ modularite.py
@@ -0,0 +1,13 @@
+'''
+modularité : utiliser les modules
+----------------
+import random
+for random import random, randrange
+from random import *
+import random as r
+randrange renvoi les nbre aleatoire selon un nbe donnée
+'''
+import random as r
+
+print(r.random())
+print(r.randrange(100))
