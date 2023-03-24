dae0e1a239b3c6e50057903f17e79af17cff2fbe
aSEMEGA <semegaaliou4@gmail.com>
2023-03-24 10:27:49 +0000

    module

---
+++ module/monModule.py
@@ -0,0 +1,10 @@
+'''
+créer ses propre module
+
+'''
+
+def carre(x):
+    return x * x
+
+def addition(x,y):
+    print(f"{x} + {y} = {x + y}")
---
+++ module/programme.py
@@ -0,0 +1,5 @@
+
+print("je suis le programme principale")
+import monModule
+
+print("le carre de 2 est de :", monModule.carre(2))
